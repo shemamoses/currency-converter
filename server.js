d7735115dc11e31719eac0a158c05f231a36a7b3
shemamoses <mosesshema1@gmail.com>
2023-06-07 20:00:44 +0200

    setting routes

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+app.post("/convert", (req, res) => {
+  res.send("you sent money to convert");
+});
+
+app.get("/", (req, res) => {
+  res.send("this is a route");
+});
+
 app.listen(port, () => {
   console.log("server is running successfully");
 });
